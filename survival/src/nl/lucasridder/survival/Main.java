4bf37d981a6813d48685d2981418b2b6918380c5
lhridder <63594063+lhridder@users.noreply.github.com>
2020-04-17 12:37:52 +0200

    versie2

--- survival/src/nl/lucasridder/survival/Main.java
+++ survival/src/nl/lucasridder/survival/Main.java
@@ -4,4 +4,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    //Start-up
+    @Override
+    public void onEnable() {
+
+    }
+
+    //Power-down
+    @Override
+    public void onDisable() {
+
+    }
+
+    //Join
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+
+    }
+
+    //Leave
+    public void onPlayerLeave(PlayerQuitEvent e ) {
+
+    }
 }
